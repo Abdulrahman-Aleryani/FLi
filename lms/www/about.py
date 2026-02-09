59549454507cda2e72e79d4b1a450cbdc0a896d5
Abdulrahman-Aleryani <abdulrahmanaleryani0@gmail.com>
2026-02-09 21:06:14 +0300

    about update

--- lms/www/about.py
+++ lms/www/about.py
@@ -3,8 +3,15 @@ import frappe
 sitemap = 1
 
 def get_context(context):
-    context.doc = frappe.get_cached_doc("About Us Settings")
-    if context.doc.is_disabled:
-        frappe.local.flags.redirect_location = "/404"
-        raise frappe.Redirect
+    try:
+        context.doc = frappe.get_cached_doc("About Us Settings")
+        if context.doc.is_disabled:
+            frappe.local.flags.redirect_location = "/404"
+            raise frappe.Redirect
+    except frappe.DoesNotExistError:
+        context.doc = frappe._dict({
+            "page_title": "About Us",
+            "company_introduction": None,
+            "is_disabled": False
+        })
     return context
